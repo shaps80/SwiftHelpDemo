4d23be070ddf75249d798482a0d2214c86e76766
Shaps Benkau <shapsuk@me.com>
2022-09-28 09:03:55 +0100

    Adds a demo project for writing sample code

---
+++ SwiftHelpDemo/SwiftHelpDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftHelpDemoApp.swift
+//  SwiftHelpDemo
+//
+//  Created by Shaps Benkau on 28/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftHelpDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
